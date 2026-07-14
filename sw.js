4337f0dd0236162ab99ac13772d353a01b1c3284
yasuki-git <285088616+yasuki-git@users.noreply.github.com>
2026-07-14 10:17:44 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "warikanpai-v5";
+const CACHE = "warikanpai-v2";
 const ASSETS = ["./", "index.html", "app.js", "manifest.json", "icon-192.png", "icon-512.png"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
